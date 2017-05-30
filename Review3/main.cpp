429d5ad524680ad7cccddd2010939b8caa06bc0a
Mauricio Matsubara <mauricio@matsubara.co>
2017-05-30 18:11:57 -0300

    exercise desc

--- Review3/main.cpp
+++ Review3/main.cpp
@@ -6,6 +6,16 @@
 //  Copyright © 2017 Mauricio Matsubara. All rights reserved.
 //
 
+/* 
+ 
+ 1. Create to float variables
+ Assign them values (you can choose)
+ If the first one is greater than the second one, print "The first is higher than the second", else print "The first is lower than the second"
+ 
+ 2. Instead of "first" and "second", print their values. 
+ 
+ */
+
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
